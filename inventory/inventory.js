55fa449928422955092b2e41162452ed44c00db9
melattesfay <melat.tesfay@amethodschools.org>
2019-06-19 22:29:41 +0000

    changes to the js

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -33,3 +33,39 @@ $("#test").click(function(){
 $("#test2").click(function(){
     $("#testDiv").text("test");
 });
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  $("#content").html(hide);
+}
+
+function closeForm() {
+  document.getElementById("myDiv").style.display = "none";
+   document.getElementById$("myForm").style.display ="none";
+ 
+}
+
+$(".open-button").click(function(){
+$("body").css("background-color", "grey");
+var x = document.getElementById("myDIV");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+$("#close-button").click(function(){
+var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+$("#close-button").click(function(){
+var y = document.getElementById("form-popup");
+  ("#myform").hide();
+});
