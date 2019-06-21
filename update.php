1ea18011bf49b8b93dc0646ec9459947046ec4c7
Paul Hulett <hulett@squareup.com>
2019-06-21 12:50:49 -0400

    add more formatting to the front of the status lines

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
 echo("Fetching from github<br>\n");
 echo exec('git fetch --all');
-echo("Resetting to the head of origin/master<br>\n");
+echo("<br>\nResetting to the head of origin/master<br>\n");
 echo exec('git reset --hard origin/master');
-echo("Done<br>\n");
+echo("<br>\n</br>Done<br>\n");
