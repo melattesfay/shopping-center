58ba6f98895156efb4903bd1abbf2bd2be82023a
melattesfay <melat.tesfay@amethodschools.org>
2019-07-02 23:03:45 +0000

    made changes to the cart

--- cart/cart.js
+++ cart/cart.js
@@ -1,4 +1,4 @@
-$("#backBtn").click(function(){
+/*$("#backBtn").click(function(){
     window.history.back();
 });
 
@@ -7,83 +7,121 @@ $("#logo").click(function(){
 });
 
 $("#checkout").click(function(){
-    window.location = "../payment/payment.html"
+    window.location = "../payment/payment.html";
 });
-
-$("#testingBtn").click(function(){
-    $("#nameCategory").empty();
-    $("#quantityCategory").empty();
-    $("#priceCategory").empty();
-});
-
-$("#testFunction").click(function(){
-    makeNewDiv();
-});
-$("#checkout").click(function(){
-    
-});
+*/
 
 window.onload = function makeNewDiv(){
     $("#nameCategory").empty();
     $("#quantityCategory").empty();
     $("#priceCategory").empty();
+    $(".firstLine3").empty();
     $.ajax({
         url: "../api/api.json",
         method: "GET",
         success: function(response){
-            var numOfItems = response.length;
+            var numOfItems = 2;
+            var prices = [];
+            var totalPrice = 0;
+            var itemIndex = [];
+        //var totalQuntity =
+            var startingTotalPrice = response[0].variations[0].price + response[1].variations[0].price;
+
+          //  var burgerPrice = [];
+          //  var friesPrice = [];
+
+           // burgerPrice.push(response[0].variations[0].price);
+         //   friesPrice.push(response[1].variations[0].price);
+
+
+
+
+            burgerPrice.push(response[0].variations[0].price);
+            $("#numOfTotal").append(startingTotalPrice);
             $("#numOfQuan").append(numOfItems);
             response.forEach(function(item){
-                $("#nameCategory").append("<h3 id=" + item.name + ">" + item.name + "</h3>");
-                   $("#demo").append("<h3 id=" + item.name + ">" + item.name + "</h3>");
-                $("#" + item.name).css("color", "red");
-                $("#quantityCategory").append("<div id='" + item.name + "Div' " + "class='quantityTest'></div>");
-                $("#" + item.name + "Div").append("<button id='" + item.name + "Minus'>" + "-" + "</button>");
+                itemIndex.push(item.name);
+
+                $("#nameCategory").append("<h3 id=" + item.name + ">" + item.name + "</h3>"); //apending the name of the item
+                $("#quantityCategory").append("<div id='" + item.name + "Div' " + "class='quantityTest'></div>"); //apend the plus and minus each time an iten name is append
+                $("#" + item.name + "Div").append("<button id='" + item.name + "Minus'>" + "-" + "</button>"); // minus button
                 $("#" + item.name + "Div").append("<input value='1' id='" + item.name + "Input'>");
-                $("#" + item.name + "Div").append("<button id='" + item.name + "Plus'>" + "+" + "</button>");
-                var price = item.variations[0].price;
-                $("#priceCategory").append("<h3 id='" + item.name + "Price' class='price'>" + price + "</h3>");
-                $("#numOfTotal").append(price);
+                $("#" + item.name + "Div").append("<button id='" + item.name +  "Plus'>" + "+" + "</button>"); // plus button
+                $("#priceCategory").append("<h3>" + item.variations[0].price + "</h3>");
+
+
                 var currentNum = 1;
                 $("#" + item.name + "Plus").click(function(){
                     numOfItems = numOfItems + 1;
                     currentNum = currentNum + 1;
-                    price = price + item.variations[0].price;
-                    $("#" + item.name + "Input").val(currentNum);
-                    $("#numOfQuan").empty();
-                    $("#" + item.name + "Price").empty();
+                   $("#" + item.name + "Input").val(currentNum); // changing the number of qantity that are inside the box
+                   $("#numOfQuan").empty();  //empty the quantity number
+                    //$("#" + item.name + "Price").empty(); // first empty the price
+                    //$("#numOfTotal").empty();
+                    $("#numOfQuan").append(numOfItems);// append number of quantity
+                    burgerPrice.push(burgerPrice[0] + response[0].variations[0].price);
+                    burgerPrice.shift();
                     $("#numOfTotal").empty();
-                    $("#numOfQuan").append(numOfItems);
-                    $("#" + item.name + "Price").append(price);
-                    $("#numOfTotal").append(price)
+                    $("#numOfTotal").append(burgerPrice);
+                    //prices.push(item.variations[0].price); //push the price of b
+                    //prices.push(item.variations[1].price);// push the price of f
+                    //prices[0] = prices[0] + item.variations[0].price; // getting the info of the price of [0] and anding it to the price
+                    //$("#numOfTotal").append(prices[0]); // appending the price to the number of total
                 });
 
                 $("#" + item.name + "Minus").click(function(){
                     numOfItems = numOfItems - 1;
                     currentNum = currentNum - 1;
-                    price = price - item.variations[0].price;
-                    $("#" + item.name + "Input").val(currentNum);
+                    $("#" + item.name + "Input").val(currentNum);// changes the quantity
+                    $("#" + item.fries + "Input").val(currentNum);
                     $("#numOfQuan").empty();
-                    $("#" + item.name + "Price").empty();
+                   // $("#numOfTotal").empty();
                     $("#numOfQuan").append(numOfItems);
-                    $("#" + item.name + "Price").append(price);
+                    //prices.push(item.variations[0].price);
+                    //prices.push(item.variations[1].price);
+                    //prices[0] = prices[0] - item.variations[0].price;
+                    //$("#numOfTotal").append(prices[0]);
                 });
 
 
             });
 
-
-
         }
-
     });
 }
 
-
-
-$("#delete1").click(function(){
+/*$("#delete1").click(function(){
   $("#lineFirst").remove();
   $("#firstLine2").remove();
   $("#firstLine3").remove();
   $("#delete1").remove();
 });
+*/
+
+var burgerPrice = [];
+var friesPrice = [];
+
+ function totalPrice(){
+    burgerPrice.push(response[0].variations[0].price);
+        $.ajax({
+            url: "../api/api.json",
+            method: "GET",
+            success: function(response){
+            $("#BurgerPlus").click(function(){
+                burgerPrice.push(burgerPrice[0] + response[0].variations[0].price);
+                burgerPrice.shift();
+                  $("#numOfTotal").empty();
+                  $("#numOfTotal").append(burgerPrice);
+            });
+            $("#BurgerMinus").click(function(){
+                burgerPrice.push(burgerPrice[0] - response[0].variations[0].price);
+                burgerPrice.shift();
+                $("#numOfTotal").empty();
+                $("#numOfTotal").append(burgerPrice);
+
+        });
+   }
+
+ });
+ }
+
