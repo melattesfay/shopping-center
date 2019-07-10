5ec637c43bcf60e75a3431586027c5e01633ca56
Paul Hulett <hulett@squareup.com>
2019-07-10 15:46:47 -0700

    "easy" self-updating code by including the vendor files
    this is a terrible idea. never do this in your production code. I'm sorry.

--- update.php
+++ update.php
@@ -3,6 +3,4 @@ echo("Fetching from github<br>\n");
 echo exec('git fetch --all');
 echo("<br>\nResetting to the head of origin/master<br>\n");
 echo exec('git reset --hard origin/master');
-echo("<br>\nUpdating Composer Dependencies<br>\n");
-echo exec('php composer.phar install');
 echo("<br>\n</br>Done<br>\n");
