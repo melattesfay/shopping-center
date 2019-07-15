af3e35bf586215f4c7eecc5e4a40018f5af25078
AndresScripted <anhuitzilhuz@s.sfusd.edu>
2019-07-15 20:39:58 +0000

    testing error messages

--- js/paymentForm.js
+++ js/paymentForm.js
@@ -57,11 +57,11 @@ callbacks: {
         errors.forEach(function (error) {
             console.error('  ' + error.message);
         });
-        alert('Could not proccess your card. Try again.');
+        //alert('Could not proccess your card. Try again.');
         return;
     }
 
-    alert("Thanks for your money!")
+    //alert("Thanks for your money!")
     // Uncomment the following block to
     // 1. assign the nonce to a form field and
     // 2. post the form to the payment processing handler
@@ -89,17 +89,17 @@ function chargeOrder(nonce){
         contentType: "application/json",
         dataType: "json",
         success: function(response){
-            /*
+
             $("#form-container").empty();
             $("#form-container").append("<h1> Thanks for the money! </h1>");
             console.log(response);
-            */
+
         },
         error: function () {
-            /*
+
             $("#form-container").empty();
-            $("#form-container").append("<h1> ERROR </h1>");
-            */
+            $("#form-container").append("<h1> ERROR! Refresh! </h1>");
+
         },
 
      });
